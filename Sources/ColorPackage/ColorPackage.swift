12697ed7e434f6601133b09949e276def91c4151
antonio.virgone <antonio.virgone@wisemotions.com>
2023-03-10 15:46:53 +0100

    wip

--- Sources/ColorPackage/ColorPackage.swift
+++ Sources/ColorPackage/ColorPackage.swift
@@ -6,7 +6,7 @@ public struct ColorPackage {
     public init() {
     }
 
-    func getColorFromRGB(r: Int, g: Int, b: Int) -> Color {
+    public func getColorFromRGB(r: Int, g: Int, b: Int) -> Color {
         return Color(red: Double(r)/Double(255), green: Double(g)/Double(255), blue: Double(b)/Double(255))
     }
 
