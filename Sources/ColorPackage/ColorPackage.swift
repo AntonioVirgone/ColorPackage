ffbe4f9ca396667db974908b7647be1a2fc15863
antonio.virgone <antonio.virgone@wisemotions.com>
2023-03-10 15:38:56 +0100

    add fun

--- Sources/ColorPackage/ColorPackage.swift
+++ Sources/ColorPackage/ColorPackage.swift
@@ -1,6 +1,13 @@
+import SwiftUI
+
 public struct ColorPackage {
     public private(set) var text = "Hello, World!"
 
     public init() {
     }
+
+    func getColorFromRGB(r: Int, g: Int, b: Int) -> Color {
+        return Color(red: Double(r)/Double(255), green: Double(g)/Double(255), blue: Double(b)/Double(255))
+    }
+
 }
